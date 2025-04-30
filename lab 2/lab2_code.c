826ee6caf666fafcf9d7e3e0d58cda2e71fbf759
NusaibaRashid <nusaibarashid17@gmail.com>
2025-04-30 20:47:31 +0530

    Completed and renamed Hello.C to lab2_code.c

--- lab 2/lab2_code.c
+++ lab 2/lab2_code.c
@@ -72,4 +72,28 @@ printf("c/=b: %d\n",c);
 c%=b;
 printf("c%%=b: %d\n",c);
 
+// Increment/Decrement Operators
+printf("Increment/Decrement Operators:\n");
+printf("a++ = %d\n", a++);
+printf("Now a = %d\n", a);
+printf("++a = %d\n", ++a);
+printf("a-- = %d\n", a--);
+printf("Now a = %d\n", a);
+printf("--a = %d\n\n", --a);
+
+// Conditional (Ternary) Operator
+printf("Conditional Operator:\n");
+int max = (a > b) ? a : b;
+printf("Max of a and b: %d\n\n", max);
+// sizeof Operator
+printf("sizeof Operator:\n");
+printf("Size of int: %d bytes\n", sizeof(int));
+printf("Size of float: %f bytes\n", sizeof(float));
+printf("Size of char: %c bytes\n\n", sizeof(char));
+// Address Operators
+printf("Pointer Operators:\n");
+printf("Address of a: %d\n", &a);
+
+return 0;
+
 }
